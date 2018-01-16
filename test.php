68320ce66886f64ccb458c8c56e36bca21fba68d
Alexndr <qsefthuken@gmail.com>
2018-01-16 12:06:23 +0300

    Added test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Тестовая страница страница");
+?>
+
+<div class="col-md-6">
+	<p>Hello, pitUh :)</p>
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
